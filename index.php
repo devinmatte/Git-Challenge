0392da4544cb90205a10153c26110c29ae9c81e1
Devin Matte <devinmatte@gmail.com>
2017-08-16 21:02:57 -0400

    Adding Language

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php ?>
 <!DOCTYPE HTML>
-<!--
-    Template:
-    Stellar by HTML5 UP
-    html5up.net | @ajlkn
-    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
-<html>
+<html lang="en">
 <head>
     <title>Git Challenge</title>
     <meta charset="utf-8"/>
